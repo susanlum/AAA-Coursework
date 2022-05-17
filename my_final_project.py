0b12211d9d13695e6a9eb8c1bb334ca0bb3437e1
susanlum <105427308+susanlum@users.noreply.github.com>
2022-05-17 18:38:19 +0800

    Update my_final_project.py

--- my_final_project.py
+++ my_final_project.py
@@ -40,8 +40,8 @@ st.subheader('Class labels and their corresponding index number')
 st.write(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
+#st.write(iris.target_names[prediction])
 st.write(prediction)
 
-#st.subheader('Prediction Probability')
+st.subheader('Prediction Probability')
 st.write(prediction_proba)
