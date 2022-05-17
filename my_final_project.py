de0ce5edb5ecf62af4a7f46b0bd54c4d1114a469
susanlum <105427308+susanlum@users.noreply.github.com>
2022-05-17 18:33:18 +0800

    Update my_final_project.py

--- my_final_project.py
+++ my_final_project.py
@@ -37,7 +37,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
 
 st.subheader('Prediction')
 st.write(iris.target_names[prediction])
